dc237b13fc91f696fd8cbec7dd9df88e89e1d919
April Vanderslice <69222543+VandersliceApril-FS@users.noreply.github.com>
2022-01-04 10:27:21 -0600

    Added in body parser dependency to package json and update route.

--- src/controllers/quizzes.js
+++ src/controllers/quizzes.js
@@ -1,6 +1,8 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
 let quizzes = require('../models/quizzes')
+const bodyParser = require('body-parser')
+router.use(bodyParser.urlencoded({ extended: false }))
 
 // routes should be around quizzes entity
 router.get('/', (req, res) => {
@@ -8,18 +10,42 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    res.send("Quizzes#Create")
+    const { id, name } = req.body
+    
+    //get data from the request using req.body and body-parser
+    quizzes.push({
+        id: Number(id),
+        name
+    })
+    res.json(quizzes)
 })
 
 router.get('/:id', (req, res) => {
-    res.send("Quizzes#Show")
+    const id = req.params.id
+    // grab quizzes then find quiz with that id
+    const quiz = quizzes.find(quiz => quiz.id == id) //loop through quizzes then filter out based on true or false
+    res.json(quiz)
 })
 
 router.post('/:id', (req, res) => {
-    res.send("Quizzes#Update")
+    const id = Number(req.params.id)
+    //map through and replace
+    quizzes.map((q) => {
+        if(id === q.id) {
+            q.name = req.body.name
+        }
+        return q
+    })
+    res.json(quizzes)
 })
 
 router.delete('/:id', (req, res) => {
     res.send("Quizzes#Delete")
 })
 module.exports = router
+
+// post form data with curl:
+// curl -X POST --data "id=6&name=ASL Quiz 6" http://localhost:3000/quizzes
+
+// update existing quiz data with id
+// curl -X POST --data "id=3&name=ASL Test 3" http://localhost:3000/quizzes/3
