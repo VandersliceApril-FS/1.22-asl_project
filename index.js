227f6b9ca7cad67947294f4fc5ba3113b116eb34
April Vanderslice <69222543+VandersliceApril-FS@users.noreply.github.com>
2022-01-04 10:03:08 -0600

    All routes set up to be RESTful

--- index.js
+++ index.js
@@ -1,40 +1,13 @@
-// load in the framework
-const { response } = require('express')
 const express = require('express')
-
-// invoke an instance of express framework
 const app = express()
+const quizzesCtrl = require('./src/controllers/quizzes')
 
-// setup homepage route: GET / HTTP/1.1
-// Catch this in express
+
 app.get('/', (request, response) => {
     response.send('Home Page GET')
-}) // pass path, callback
-
-// to test this post route use curl in terminal
-app.post('/', (request, response) => {
-    response.send('Home Page POST' )
 })
 
-app.get('/products', (request, response) => {
-    response.send('All products')
-})
-
-app.post('/products', (request, response) => {
-    response.send('Created a new product.')
-})
-
-app.get('/products/:productId', (request, response) => {
-    response.send('Updated a product with id ' + request.params.productId)
-})
-
-// Dynamic route data
-// GET /products/nike-large-white-shoe HTTP/1.1
-// : tells express what comes next is dynamic data
-app.get('/products/:productName', (request, response) => {
-    response.send('Product Page. Product name: ' + request.params.productName)
-})
-
-
+//mount router at certain endpoint. 
+app.use('/quizzes', quizzesCtrl)
 // tell the app to listen to requests on a port
 app.listen(3000)
---
+++ src/controllers/quizzes.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var router = express.Router()
+
+// routes should be around quizzes entity
+router.get('/', (req, res) => {
+    res.send("Quizzes#Index")
+})
+
+router.post('/', (req, res) => {
+    res.send("Quizzes#Create")
+})
+
+router.get('/:id', (req, res) => {
+    res.send("Quizzes#Show")
+})
+
+router.post('/:id', (req, res) => {
+    res.send("Quizzes#Update")
+})
+
+router.delete('/:id', (req, res) => {
+    res.send("Quizzes#Delete")
+})
+module.exports = router
