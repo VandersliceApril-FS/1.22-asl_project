3d48a0ad666cfd63a458b6ce58a29af03d184588
April Vanderslice <69222543+VandersliceApril-FS@users.noreply.github.com>
2022-01-21 09:33:13 -0600

    Starting to add React styling.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const quizzesCtrl = require('./src/controllers/quizzes')
 const questionsCtrl = require('./src/controllers/questions')
 const choicesCtrl = require('./src/controllers/choices')
 const authCtrl = require('./src/controllers/auth')
+
 const session = require('express-session')
 app.use(session({
     saveUninitialized: false,
@@ -11,19 +12,16 @@ app.use(session({
     cookie: { maxAge: 60000 }
 }))
 
-app.set('views', __dirname + '/src/views') //current directory that this file is in
-// set view engine to twig
+app.set('views', __dirname + '/src/views')
 app.set('view engine', 'twig')
 
-app.get(
-    '/', (request, response, next) => { response.render('home/home')}
-)
+app.use(express.static('public'))
 
-//mount router at certain endpoint. 
+app.get('/', (request, response, next) => { response.render('home/home')})
+
 app.use('/quizzes', quizzesCtrl)
 app.use('/questions', questionsCtrl)
 app.use('/choices', choicesCtrl)
 app.use('/auth', authCtrl)
 
-// tell the app to listen to requests on a port
 app.listen(3000)
