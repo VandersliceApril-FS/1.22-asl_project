0b697085e81d9009dc4290e88de789f9aaa83339
April Vanderslice <69222543+VandersliceApril-FS@users.noreply.github.com>
2022-01-26 14:56:05 -0600

    Mostly the same files: many things were moved around in an attempt to figure out how to log the user out.

--- quizzes-app/src/App.js
+++ quizzes-app/src/App.js
@@ -2,23 +2,22 @@ import './App.css'
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import {
-  BrowserRouter as Router,
+  Routes,
   Route,
-  Routes
+  BrowserRouter as Router
 } from 'react-router-dom'
-import Home from './Home'
-import Navigation from './Navigation'
-import Login from './Login'
+import Navigation from './components/Navigation'
+import Home from './pages/Home'
+import Logout from './pages/Logout'
 import Quiz from './Quiz'
 import queryString from 'querystring'
 
 function App() {
   const [jwt, setJwt] = useState('')
+  
   useEffect(() => {
     async function fetchJwt() {
       const params = queryString.parse(window.location.search.replace(/^\?/, ''))
-
-      console.log(`params: ${params}. params.token: ${params.token}`)
 
       localStorage.token = params.token
       const response = await axios('http://localhost:3000/auth/token/', {
@@ -32,21 +31,19 @@ function App() {
     fetchJwt()
   }, [])
 
-  // when user clicks log out, they should be rerouted to the login page and jwtState should be updated
-  if (!jwt) {
-    return <Login />;
-  }
+  
+
   return (
     <Router>  
       <div className="app-container">
-        <Navigation isLoggedIn={jwt ? true : false} />
-        <div className="main-content">
+        <Navigation isLoggedIn={jwt ? true : false} setJwt/>
+        <main className="main-content">
           <Routes>
-            <Route exact path='/' element={<Home />} />
+            <Route exact path='/' element={<Home isLoggedIn={jwt ? true : false} />} />
             <Route exact path='/quizzes/:id' element={<Quiz />} />
-            <Route exact path='/logout' element={<Home />} />
+            <Route exact path='/logout' element={<Logout />}></Route>
           </Routes>
-        </div>
+        </main>
       </div>
     </Router>
   );
