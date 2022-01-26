2bf63154c4e67bed0c22cc7a0b6aba61f3d8cbd1
April Vanderslice <69222543+VandersliceApril-FS@users.noreply.github.com>
2022-01-26 13:01:04 -0600

    Switched to a functional component.

--- quizzes-app/src/Navigation.jsx
+++ quizzes-app/src/Navigation.jsx
@@ -3,41 +3,40 @@ import { NavLink } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
-class Navigation extends React.Component
-{
-    render() {
-        return(
-            <header>
-                <Navbar style={{ margin: "0 2rem" }}>                   
-                    <Navbar.Brand>
-                        <NavLink 
-                            to='/'
-                            style={{
-                                color: "black",
-                                textDecoration: "none"
-                            }}
-                        >
-                            Home
-                        </NavLink>
-                    </Navbar.Brand>
-                    <Nav>
-                    {this.props.isLoggedIn && 
-                        <NavLink 
-                            to='/logout'
-                            style={{
-                                color: "grey",
-                                textDecoration: "none"
-                            }}
-                        >
-                            Logout
-                        </NavLink>
-                    }
-                    </Nav>
-                    
-                </Navbar>
-            </header>
-        )
-    }
+function Navigation({ isLoggedIn }) {
+    
+    return(
+        <header>
+            <Navbar style={{ margin: "0 2rem" }}>                   
+                <Navbar.Brand>
+                    <NavLink 
+                        to='/'
+                        style={{
+                            color: "black",
+                            textDecoration: "none"
+                        }}
+                    >
+                        Home
+                    </NavLink>
+                </Navbar.Brand>
+                <Nav>
+                {/* if user is logged in, display the logout button. */}
+                {isLoggedIn && 
+                    <NavLink 
+                        to='/logout'
+                        style={{
+                            color: "grey",
+                            textDecoration: "none"
+                        }}
+                    >
+                        Logout
+                    </NavLink>
+                }
+                </Nav>
+                
+            </Navbar>
+        </header>
+    )
 }
 
 export default Navigation
